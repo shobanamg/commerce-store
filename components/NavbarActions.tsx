2eba8280d53ff5c02cdec7c6635881151b278587
Shobana Mathiarul <mathiarulshobana@gmail.com>
2023-10-28 06:54:27 +0200

    Added shopping bag icon from lucide-rect library

--- components/NavbarActions.tsx
+++ components/NavbarActions.tsx
@@ -1,7 +1,29 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
+import Button from '@/components/ui/Button';
+import { ShoppingBag } from '@/node_modules/lucide-react';
+
 const NavbarActions = () => {
-  return <div className="ml-auto flex items-center gap-x-4">Hello Actions</div>;
+  const [ismounted, setismounted] = useState(false);
+
+  useEffect(() => {
+    setismounted(true);
+  }, []);
+
+  if (!ismounted) {
+    return null;
+  }
+
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag size={20} color="white" />
+        <span className="ml-2 text-sm font-medium text-white"> 0 </span>
+      </Button>
+    </div>
+  );
 };
 
 export default NavbarActions;
